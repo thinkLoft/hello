d655d1e32ef7c7507b5b847c3e53d3b7fc8bb1a9
thinkLoft <kareem.lofters@gmail.com>
2018-12-01 21:46:44 -0500

    apiRoutes: added checkDB, autoAdsRSs, AutoAdsScraper

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,8 +2,52 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const router = require("express").Router();
 
+// Require all models
+const db = require("../models");
+
 // // Route for getting all Articles from the db
-router.get("/crawl", function(req, res) {
+router.get("/autoAdsRss", function(req, res) {
+  autoAdsRss();
+  res.send("complete");
+});
+
+router.get("/unscrapedAds", function(req, res) {
+  db.Post.find({ postTitle: undefined })
+    .then(function(post) {
+      post.forEach(function(i, element) {
+        autoAdsScraper(i.srcURL);
+      });
+
+      res.send("Scrape Successful");
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+module.exports = router;
+
+// ==========================
+// ======= Functions ========
+// ==========================
+
+// CHECK IF URL IS IN DATABASE
+function checkDB(result) {
+  db.Post.find({ srcURL: result.srcURL }, function(err, docs) {
+    if (docs.length) {
+      // no ad found
+    } else {
+      console.log("Ad Found!");
+      db.Post.create(result).catch(function(err) {
+        console.log(err);
+      });
+    }
+  });
+}
+
+// CHECKS AUTO ADS FOR LATEST ADS
+function autoAdsRss() {
   axios.get("https://www.autoadsja.com/rss.asp").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data, { xmlMode: true });
@@ -12,23 +56,92 @@ router.get("/crawl", function(req, res) {
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
-      result.link = $(this)
+      result.srcURL = $(this)
         .children("link")
         .text();
-      result.title = $(this)
+      result.srcTitle = $(this)
         .children("title")
         .text();
-      result.img = $(this)
+      result.srcImg = $(this)
         .children("description")
         .text();
       ans.push(result);
+
+      // Check
+      checkDB(result);
     });
-    res.send(ans);
+
+    return "Grabbed Latest Auto Ads RSS";
   });
-});
+}
 
-module.exports = router;
+function autoAdsScraper(link) {
+  axios.get(link).then(function(response) {
+    console.log(response.data.url);
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
 
-// ==========================
-// ======= Functions ========
-// ==========================
+    // Save an empty result object
+    var result = {};
+
+    // crawled variables
+    var title = $(".price-tag > h1").text();
+    var price = $(".price-tag > h2")
+      .text()
+      .replace(/[^0-9.-]+/g, "");
+    // Add Formatted price to Title
+    title += " - $" + price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+    var ymm = title.split(" "); // break Title into array of text
+    var year = ymm[0];
+    var make = ymm[1];
+    var modelIndex = title.indexOf(make) + make.length + 1;
+    var model = title.substring(modelIndex).replace(/\$.*/g, "");
+
+    var location = $(".per-detail > ul > li")[0]
+      .children[0].data.replace("Location: ", "")
+      .replace(/\s+/g, "")
+      .replace(".", ". ");
+
+    var contact = $(".contact_details")
+      .text()
+      .replace(/[^0-9]+/g, "")
+      .substring(0, 11);
+
+    // Get Features for description
+    var features = [];
+
+    features.push($(".vehicle-description").text());
+
+    $(".per-detail > ul > li").each(function(i) {
+      features.push($(this).text());
+    });
+
+    features.push($(".contact_details").text());
+
+    var description = "";
+    features.forEach(function(element) {
+      description += element.toString();
+      description += "\n";
+    });
+
+    // Get Images
+    var imgs = [];
+    $(".product-images > .prod-box > a").each(function(i) {
+      imgs.push($(this).attr("href"));
+    });
+
+    // Update Results object
+    result.title = title;
+    result.price = price;
+    result.year = year;
+    result.make = make;
+    result.model = model;
+    result.parish = location;
+    result.contactNumber = contact;
+    result.description = description;
+    result.imgs = imgs;
+    result.price = price;
+    result.posted = false;
+  });
+}
