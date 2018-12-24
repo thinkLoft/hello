5ba8b259623b976ea16cab7c6ea9e0a1c5fd0356
thinkLoft <kareem.lofters@gmail.com>
2018-12-24 03:21:11 -0500

    apiRoutes - Dried up code, reduced routes, compressed checker and scraper into one function, finished new crawler

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,18 +14,8 @@ const db = require("../models");
 // ======== Routes ==========
 // ==========================
 // // Route for getting all Articles from the db
-router.get("/crawl", async function(req, res) {
-  var ret = await checker();
-  res.send(ret);
-});
-
-router.get("/scrape", function(req, res) {
-  var ret = verify();
-  res.send(ret);
-});
-
-router.get("/post", function(req, res) {
-  var ret = ifPosted();
+router.get("/crawler", async function(req, res) {
+  var ret = checker();
   res.send(ret);
 });
 
@@ -66,9 +56,7 @@ function checker() {
           // no ad found
         } else {
           console.log("Ad Found!");
-          db.Post.create(result).catch(function(err) {
-            console.log(err);
-          });
+          scraper(result.srcURL);
         }
       });
       // end post function
@@ -77,39 +65,6 @@ function checker() {
   });
   // end of axios function
   return "hello from getAutoAdsLinks function";
-}
-
-// A1 - AUTOADS RSS VERIFIER
-function verify() {
-  db.Post.find({ postTitle: undefined })
-    .then(function(post) {
-      post.forEach(function(i, element) {
-        console.log("ad scraped");
-        console.log(i.srcURL);
-        scraper(i.srcURL);
-      });
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.send(err);
-    });
-
-  return "Verification complete";
-}
-
-// A2 - AUTOADS POST VERIFIER
-function ifPosted() {
-  db.Post.find({ posted: false }).then(async function(res) {
-    count = 0;
-    for (let i of res) {
-      count++;
-      console.log("---:" + count);
-      console.time("asyncPoster");
-      await asyncPoster(i);
-      console.timeEnd("asyncPoster");
-    }
-    console.log(res.length);
-  });
 }
 
 // B - SCRAPER: AUTOADS
@@ -172,8 +127,7 @@ function scraper(link) {
     });
 
     // Update Results object
-    var srclink = response.config.url;
-
+    result.srcURL = response.config.url;
     result.postTitle = title;
     result.price = price;
     result.year = year;
@@ -186,23 +140,73 @@ function scraper(link) {
     result.price = price;
     result.posted = false;
 
-    db.Post.findOneAndUpdate({ srcURL: srclink }, result).catch(err =>
-      console.log(err)
-    );
+    // create new row in database
+    db.Post.create(result).catch(err => console.log(err));
   });
-  return "hello from the crawler";
   // end of crawler
+  console.log("adScraped!");
 }
 
-// C - Scheduler
-const job = new CronJob("0 */5 * * * *", async function() {
-  await setTimeout(async function() {
-    await checker();
-  }, 5000);
-
-  await verify();
+// C - SCHEDULER
+const job = new CronJob("0 */15 * * * *", async function() {
+  checker();
   console.log("Cron Run");
 });
 
+// D - SCRAPER: Jamiaca Cars
+function pageScraper(link) {
+  axios.get(link).then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+
+    // page Crawler
+    $(".hiddenInfo").each(function(i, element) {
+      // Save an empty result object
+      var result = {};
+
+      result.srcURL = $(this)
+        .children("a")
+        .attr("href");
+
+      // Update Results object
+      var srclink = response.config.url;
+
+      // result.postTitle = title;
+      // result.price = price;
+      // result.year = year;
+      // result.make = make;
+      // result.model = model;
+      // result.parish = location;
+      // result.contactNumber = contact;
+      // result.description = description;
+      // result.imgs = imgs;
+      // result.price = price;
+      result.posted = false;
+      console.log(result);
+    });
+  });
+
+  return 1;
+  // end of crawler
+}
+
 // LAUNCHER
 job.start();
+
+// Crawler - Jamaica Cars
+function pageCrawler() {
+  var count = 0;
+  var baseURL = "https://www.jacars.net/?page=browse&e=AddedThisWeek&p=";
+
+  var targetURL = baseURL + count;
+
+  // Only scrapes first 10 pages
+  while (count < 10) {
+    count++;
+    targetURL = baseURL + count;
+    pageScraper(targetURL);
+  }
+}
+
+// TEMP LAUNCHER
+// pageCrawler();
